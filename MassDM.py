7e3d3cb333b6786ca616a7ef16ee2f3b647f1f68
Infamy <65705916+freeloot@users.noreply.github.com>
2021-10-18 11:42:57 +0100

    Update MassDM.py

--- MassDM.py
+++ MassDM.py
@@ -1,4 +1,4 @@
-#Libraries
+
 import discord
 import time
 import ctypes
@@ -6,7 +6,6 @@ import click
 import os
 import json
 
-#Program
 windowSize = 'mode 70,20'
 os.system(windowSize)
 ctypes.windll.kernel32.SetConsoleTitleW("Infamy.py")
@@ -19,41 +18,42 @@ class colours:
 	GREEN = '\033[32m' 
 	WHITE = '\033[37m'
 	DEFUALT = '\033[m'
-	BLACK = '\033[30m' # Kinda Usesless for black terminals
+	BLACK = '\033[30m'
 	RED = '\033[31m'
 	BLUE = '\033[34m'
 	YELLOW = '\033[33m'
 
 def Main():
 
-	def start():
-		global auth
-		auth = None
-		filename = 'token.json'
-		with open(filename) as f:
-			config = json.load(f)
-			auth = config.get('token')
 
-		print(colours.GREEN + """██╗███╗░░██╗███████╗░█████╗░███╗░░░███╗██╗░░░██╗
-██║████╗░██║██╔════╝██╔══██╗████╗░████║╚██╗░██╔╝
-██║██╔██╗██║█████╗░░███████║██╔████╔██║░╚████╔╝░
-██║██║╚████║██╔══╝░░██╔══██║██║╚██╔╝██║░░╚██╔╝░░
-██║██║░╚███║██║░░░░░██║░░██║██║░╚═╝░██║░░░██║░░░
-╚═╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░╚═╝░░░
+	global auth
+	auth = None
+	filename = 'token.json'
+	with open(filename) as f:
+		config = json.load(f)
+		auth = config.get('token')
+
+		print(colours.GREEN + """ ___  ________   ________ ________  _____ ______       ___    ___ 
+|\  \|\   ___  \|\  _____\\   __  \|\   _ \  _   \    |\  \  /  /|
+\ \  \ \  \\ \  \ \  \__/\ \  \|\  \ \  \\\__\ \  \   \ \  \/  / /
+ \ \  \ \  \\ \  \ \   __\\ \   __  \ \  \\|__| \  \   \ \    / / 
+  \ \  \ \  \\ \  \ \  \_| \ \  \ \  \ \  \    \ \  \   \/  /  /  
+   \ \__\ \__\\ \__\ \__\   \ \__\ \__\ \__\    \ \__\__/  / /    
+    \|__|\|__| \|__|\|__|    \|__|\|__|\|__|     \|__|\___/ /     
+                                                     \|___|/ 
 [Support] discord.gg/2NWpB3vRdy
 		""" + colours.DEFUALT)
 
-		print(" ")
-		print(colours.WHITE + 'ᴡᴇʟᴄᴏᴍᴇ ᴘʟᴀʏᴇʀ.')
-		print('ᴜsᴇ ᴀᴛ ʏᴏᴜʀ ᴏᴡɴ ʀɪsᴋ !')
-		print('')
-		print('')
-		print('1) MassDM Friends list')
-		print('')
-		global choice
-		choice = input("-> ")
+	print(" ")
+	print(colours.WHITE + 'ᴡᴇʟᴄᴏᴍᴇ ᴘʟᴀʏᴇʀ.')
+	print('ᴜsᴇ ᴀᴛ ʏᴏᴜʀ ᴏᴡɴ ʀɪsᴋ !')
+	print('')
+	print('')
+	print('1) MassDM Friends list')
+	print('')
+	global choice
+	choice = input("-> ")
 
-	start()
 
 	def dmfriends():
 		message = input('-> Enter Message: ')
@@ -70,9 +70,12 @@ def Main():
 					print(f"-> unable to message {user.name}")
 			input('Execution Completed...\n')
 			click.clear()
-
+		
 	if choice == '1':
 		dmfriends()
+	elif choice == "2":
+		print('testing')
+
 
 	client.run(auth, bot=False)
 Main()
